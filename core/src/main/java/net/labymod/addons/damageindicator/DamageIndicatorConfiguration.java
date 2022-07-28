f107f3ac1a36981c2f7949ba8af669430a58de46
Nico <jumpingpxl@gmail.com>
2022-07-28 16:58:11 +0200

    update addon to the latest API

--- core/src/main/java/net/labymod/addons/damageindicator/DamageIndicatorConfiguration.java
+++ core/src/main/java/net/labymod/addons/damageindicator/DamageIndicatorConfiguration.java
@@ -1,33 +1,35 @@
 package net.labymod.addons.damageindicator;
 
+import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.dropdown.DropdownWidget.DropdownSetting;
-import net.labymod.api.configuration.loader.Config;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
+import net.labymod.api.configuration.loader.property.ConfigProperty;
 
 /**
  * The damage indicator configuration.
  */
 @ConfigName("settings")
-@SuppressWarnings("FieldMayBeFinal")
-public final class DamageIndicatorConfiguration extends Config {
+public final class DamageIndicatorConfiguration extends AddonConfig {
 
   @SwitchSetting
-  private boolean enabled = true;
+  private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
   @DropdownSetting
-  private DisplayType displayType = DisplayType.HEALTH_BAR;
+  private final ConfigProperty<DisplayType> displayType = new ConfigProperty<>(
+      DisplayType.HEALTH_BAR);
 
-  public boolean isEnabled() {
+  @Override
+  public ConfigProperty<Boolean> enabled() {
     return this.enabled;
   }
 
-  public DisplayType getDisplayType() {
+  public ConfigProperty<DisplayType> displayType() {
     return this.displayType;
   }
 
   public boolean isVisible(DisplayType value) {
-    return this.isEnabled() && this.displayType == value;
+    return this.enabled().get() && this.displayType.get() == value;
   }
 
   public enum DisplayType {
